e2ac6e4b4ac06d5ebc14ad5b4d42298abc3dcee7
Nadzhmeddin <xside55@yandex.ru>
2025-12-03 15:15:48 +0300

    Library project

---
+++ src/main/java/ru/project/library/entity/MyUser.java
@@ -0,0 +1,13 @@
+package ru.project.library.entity;
+
+import ru.project.library.enums.UserRole;
+
+public class MyUser {
+
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private UserRole role;
+}
